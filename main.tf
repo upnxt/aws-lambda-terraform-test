c6d0889c31c11931e96a4fe965e56d715dd5fb92
upnxt <upnxt@users.noreply.github.com>
2022-04-21 10:55:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 variable "sg_ids" {}
 variable "subnet_ids" {}
+variable "s3_bucket" {}
+variable "s3_key" {}
 
 terraform {
   required_providers {
@@ -63,18 +65,13 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
-data "archive_file" "zip_the_python_code" {
-  type = "zip"
-  source_dir  = "${path.module}/src/"
-  output_path = "${path.module}/src/hello.zip"
-}
-
 resource "aws_lambda_function" "terraform_lambda_func" {
-  filename = "${path.module}/src/hello.zip"
-  function_name = "Test_Lambda_Function"
+  s3_bucket = var.s3_bucket
+  s3_key = var.s3_key
+  function_name = "AWSLambda2"
   role = aws_iam_role.lambda_role.arn
-  handler = "index.lambda_handler"
-  runtime = "python3.8"
+  handler = "AWSLambda1::AWSLambda1.Function::FunctionHandler"
+  runtime = "dotnet6"
   depends_on = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
   
   vpc_config {
